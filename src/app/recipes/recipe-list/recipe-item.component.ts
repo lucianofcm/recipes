88ba048c1ecf999c86e02a7144693cfcbf455916
luciano <lucweb@gmail.com>
2017-03-06 18:16:01 -0300

    dsfasdfa

---
+++ src/app/recipes/recipe-list/recipe-item.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe'
+
+@Component({
+  selector: 'rb-recipe-item',
+  templateUrl: './recipe-item.component.html',
+  styleUrls: ['./recipe-item.component.css']
+})
+export class RecipeItemComponent implements OnInit {
+
+  recipe: Recipe;
+  recipeID: number;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
