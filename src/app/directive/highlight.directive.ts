e9130fb604d0f5d06c0a18a50b4c06c123c62c27
lucianofcm <lucweb@gmail.com>
2017-03-15 07:04:48 -0300

    sdafdfd

--- src/app/directive/highlight.directive.ts
+++ src/app/directive/highlight.directive.ts
@@ -9,17 +9,31 @@ export class HighlightDirective {
      this.elementRef.nativeElement.style.backgroundColor = 'green';
      this.renderer.setElementStyle(this.elementRef.nativeElement, 'background-color', 'red')
    }*/
-
-
-  @HostListener('mouseenter') mouseover() {
+private backgroundColor = 'white';
+private fontSize = '3em';
+  
+/*  @HostListener('mouseenter') mouseSobre() {
     this.backgroundColor = 'yellow';
+  };*/
+  /*  @HostListener('mouseenter') mouseSobreFont() {
+    this.fontSize= 30;
   };
   @HostListener('mouseleave') mouseleave() {
     this.backgroundColor = 'red';
   };
   @HostBinding('style.backgroundColor') get setColor() {
     return this.backgroundColor;
+  }*/
+
+  @HostListener('mouseenter') mouseSobreFont() {
+    this.fontSize= '5em';
+  };
+    @HostListener('mouseleave') mouseFora() {
+    this.fontSize= '3em';
+  };
+  @HostBinding('style.font-size') get setFontSize() {
+    return this.fontSize;
   }
-  private backgroundColor = 'white';
+  
   constructor() { }
 }
